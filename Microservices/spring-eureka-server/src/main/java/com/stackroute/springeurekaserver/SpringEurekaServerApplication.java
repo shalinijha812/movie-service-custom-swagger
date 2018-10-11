8af69a246db388e79654d98072339da0c68d5ddd
shalini <shalinijha812@gmail.com>
2018-10-11 23:57:17 +0530

    with validation

---
+++ Microservices/spring-eureka-server/src/main/java/com/stackroute/springeurekaserver/SpringEurekaServerApplication.java
@@ -0,0 +1,12 @@
+package com.stackroute.springeurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringEurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringEurekaServerApplication.class, args);
+	}
+}
